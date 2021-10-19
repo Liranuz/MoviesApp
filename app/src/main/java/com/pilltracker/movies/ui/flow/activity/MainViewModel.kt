77f5dbf8aa302ccfba18e2c1acf5fca99ab083af
Liran Ushpiz <liran@pilltracker.com>
2021-10-19 17:18:10 +0300

    removed comments

--- app/src/main/java/com/pilltracker/movies/ui/flow/activity/MainViewModel.kt
+++ app/src/main/java/com/pilltracker/movies/ui/flow/activity/MainViewModel.kt
@@ -8,28 +8,4 @@ import javax.inject.Inject
 
 class MainViewModel @Inject constructor(): BaseViewModel() {
 
-
-//    val favorites : MutableList<Result> = arrayListOf()
-//
-//    private val _isExist = SingleLiveEvent<Boolean>()
-//    val getIsExit : LiveData<Boolean> = _isExist
-//
-//
-//    fun onFavoriteIconClicked(movie: Result) {
-//        val isExistInList: Result? = favorites.find { it.id == movie.id }
-//        isExistInList?.let {}?: kotlin.run {
-//            favorites.add(movie)
-//        }
-//        _isExist.value = isExistInList != null
-//    }
-//
-//
-//    fun onRemoveIconClicked(movie: Result) {
-//        favorites.remove(movie)
-//
-//    }
-//
-//
-//
-//    fun isFavorite(id: Int?): Boolean =  favorites.find { it.id == id } != null
 }
